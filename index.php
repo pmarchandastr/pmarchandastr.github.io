92f4de43c6e1d5c9142aad9c5fe5abda5da83c39
Pierre Marchand <pmarchan@bitbucket.org>
2020-09-09 17:56:56 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+
+
+<html>
+  
+  <?php include('entete.php');?>
+  
+  <body>
+  
+
+
+  <?php include('menu.php');?>
+
+
+     <div class="row">
+       <div class="column">
+
+       <h1>About me</h1>
+
+
+
+
+       <p id="welcome">
+         Welcome to my personal website !
+       </p>
+        
+
+       <p>
+         <span class="field">Name :       </span>&nbsp <span class="content"> Pierre Marchand</span></br>
+         <span class="field">Position :   </span>&nbsp <span class="content"> Postdoctoral fellow</span> </br>
+         <span class="field">Location :   </span>&nbsp <span class="content"> American Museum of Natural History, New-York</span> </br>
+         <span class="field">Research :   </span>&nbsp <span class="content"> Numerical simulations of star formation</span> </br>
+         <p>
+         <p>
+         Please visit the "Research" page to learn more about my work.
+         </p>
+         <p>
+         You can contact me at pierre.marchand.astr _at_ gmail.com (replace "_at_" by the "@" symbol).
+       </p>
+
+       </div> 
+       <div class="column">
+         <img src="myself.png" alt="Pierre Marchand" class="Picture" />
+
+     </div>
+
+     </div> <!-- corpse -->
+
+  </body>
+   
+
+</html>
